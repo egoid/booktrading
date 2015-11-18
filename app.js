f2768de0099d5ada82b16bb0ed5e2e3cb7413771
Ta <ta@Tas-MacBook-Pro.local>
2015-11-17 21:13:46 -0800

    added premature login

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Book Trading' });
 });
+
+router.post('/login', function(req,res){
+  var user = req.body
+  if (user.username === 'codinghouse@codinghouse.co' && user.password ==='werock') {
+    res.send('win')
+  } else {
+    res.send('fail')
+  }
+})
+
 
 module.exports = router;
